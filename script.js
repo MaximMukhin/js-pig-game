568de5083a0d7b2c856d13124442f33f3c8e668b
MaximMukhin <eriwer@gmail.com>
2022-10-13 20:57:45 +0300

    fix

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const scorePlayerOneElement = document.getElementById("score--0");
+const scorePlayerTwoElement = document.getElementById("score--1");
+const diceElement = document.querySelector(".dice");
+const btnNewGameElement = document.querySelector(".btn--new");
+const btnRollTheDiceElement = document.querySelector(".btn--roll");
+const btnTakeScoreElement = document.querySelector(".btn--hold");
+
+const scorePlayerOne = 0;
+const scorePlayerTwo = 0;
+
+scorePlayerOneElement.textContent = scorePlayerOne;
+scorePlayerTwoElement.textContent = scorePlayerTwo;
+diceElement.classList.add("hidden");
+
+const rollTheDice = () => {
+  const diceRandomNum = Math.trunc(Math.random() * 6 + 1);
+  console.log(diceRandomNum);
+  diceElement.classList.remove("hidden");
+  diceElement.src = `dice${diceRandomNum}.png`;
+};
+
+btnRollTheDiceElement.addEventListener("click", rollTheDice);
+
+
+
+
+
