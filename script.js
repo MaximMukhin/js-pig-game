2a19e8a85a2e1052bf13541a4182f3d9b294ec81
MaximMukhin <eriwer@gmail.com>
2022-10-16 18:46:23 +0300

    fix

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const currentScorePlayerTwoElement = document.getElementById("current--1");
 const playerOneElement = document.querySelector(".player--0");
 const playerTwoElement = document.querySelector(".player--1");
 
-let totalScore = [0, 0];
+const totalScore = [0, 0];
 // let scorePlayerOne = 0;
 // let scorePlayerTwo = 0;
 
@@ -46,8 +46,6 @@ const rollTheDice = () => {
 
 btnRollTheDiceElement.addEventListener("click", rollTheDice);
 
-btnTakeScoreElement.addEventListener("click", () => {});
-
 btnNewGameElement.addEventListener("click", () => {
   currentScore = 0;
   document.getElementById(`current--${activePlayer}`).textContent =
@@ -56,9 +54,29 @@ btnNewGameElement.addEventListener("click", () => {
   diceElement.classList.add("hidden");
   playerOneElement.classList.add("player--active");
   playerTwoElement.classList.remove("player--active");
-  totalScore = [0, 0];
+  totalScore[0] = 0;
+  totalScore[1] = 0;
+
   scorePlayerOneElement.textContent = totalScore[0];
   scorePlayerTwoElement.textContent = totalScore[1];
   console.log("NewGame");
+  console.log(totalScore);
 });
 
+btnTakeScoreElement.addEventListener("click", () => {
+  totalScore[activePlayer] += document.getElementById(
+    `current--${activePlayer}`
+  ).textContent = currentScore;
+
+  document.querySelector(`#score--${activePlayer}`).textContent =
+    totalScore[activePlayer];
+
+  console.log(totalScore);
+
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  playerOneElement.classList.toggle("player--active");
+  playerTwoElement.classList.toggle("player--active");
+});
