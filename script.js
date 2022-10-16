f1ad57d4036e61c5b4b896bb6ecada6bf9c3ee2e
MaximMukhin <eriwer@gmail.com>
2022-10-16 22:34:00 +0300

    fix

--- script.js
+++ script.js
@@ -30,23 +30,7 @@ const switchPlayer = () => {
   playerTwoElement.classList.toggle("player--active");
 };
 
-const rollTheDice = () => {
-  const diceRandomNum = Math.trunc(Math.random() * 6 + 1);
-  diceElement.classList.remove("hidden");
-  diceElement.src = `dice${diceRandomNum}.png`;
-
-  if (diceRandomNum !== 1) {
-    currentScore += diceRandomNum;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    switchPlayer();
-  }
-};
-
-btnRollTheDiceElement.addEventListener("click", rollTheDice);
-
-btnNewGameElement.addEventListener("click", () => {
+const newGame = () => {
   currentScore = 0;
   document.getElementById(`current--${activePlayer}`).textContent =
     currentScore;
@@ -67,14 +51,32 @@ btnNewGameElement.addEventListener("click", () => {
 
   console.log("NewGame");
   console.log(totalScore);
-});
+};
+
+const rollTheDice = () => {
+  const diceRandomNum = Math.trunc(Math.random() * 6 + 1);
+  diceElement.classList.remove("hidden");
+  diceElement.src = `dice${diceRandomNum}.png`;
+
+  if (diceRandomNum !== 1) {
+    currentScore += diceRandomNum;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    switchPlayer();
+  }
+};
+
+btnRollTheDiceElement.addEventListener("click", rollTheDice);
+
+btnNewGameElement.addEventListener("click", newGame);
 
 btnTakeScoreElement.addEventListener("click", () => {
   totalScore[activePlayer] += currentScore;
   document.querySelector(`#score--${activePlayer}`).textContent =
     totalScore[activePlayer];
 
-  if (totalScore[activePlayer] >= 0) {
+  if (totalScore[activePlayer] >= 200) {
     document
       .querySelector(`.player--${activePlayer}`)
       .classList.remove("player--active");
@@ -93,10 +95,19 @@ btnTakeScoreElement.addEventListener("click", () => {
 
 const modalWindowElement = document.querySelector(".modal-window");
 const overlayElement = document.querySelector(".overlay");
+const winnerTextElement = document.querySelector(".winner-text");
+const player1Score = document.querySelector(".player1-score");
+const player2Score = document.querySelector(".player2-score");
 
 const showModalWindow = () => {
   modalWindowElement.classList.remove("hidden-overlay");
   overlayElement.classList.remove("hidden-overlay");
+  winnerTextElement.textContent = `Игрок под номером ${
+    activePlayer + 1
+  } выиграл!!!`;
+
+  player1Score.textContent = `Игрок 1 набрал '${totalScore[0]}' очков`;
+  player2Score.textContent = `Игрок 2 набрал '${totalScore[1]}' очков`;
 };
 
 const btnCloseModalWindowElement = document.querySelector(
@@ -106,4 +117,7 @@ const btnCloseModalWindowElement = document.querySelector(
 const closeModalWindow = () => {
   modalWindowElement.classList.add("hidden-overlay");
   overlayElement.classList.add("hidden-overlay");
+  newGame();
 };
+
+btnCloseModalWindowElement.addEventListener("click", closeModalWindow);
